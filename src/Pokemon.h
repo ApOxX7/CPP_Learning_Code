4ad007e6066cb271380325c661abc4279d4c8f77
ApOxX <nourdinearbaoui@gmail.com>
2023-02-14 08:33:22 +0100

    Saved changes.

--- src/Pokemon.h
+++ src/Pokemon.h
@@ -1,7 +1,36 @@
 #pragma once
-
+#include <memory>
 #include <string>
 
 // A creature that is cute and can fight other ones.
 class Pokemon
-{};
+{
+private:
+    std::string _name;
+    int _id;
+    static inline int idIncr;
+
+public:
+    Pokemon(const std::string &name)
+        : _name{name}
+
+    {
+        _id = idIncr++;
+    }
+
+    Pokemon(const Pokemon &poke)
+        : _name{poke.name()}
+
+    {
+        _id = idIncr++;
+    }
+
+    std::string name() const
+    {
+        return _name;
+    }
+    int id() const
+    {
+        return _id;
+    }
+};
